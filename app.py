eb9ff6f259df318c0586fc7cb838d4c90ce9c1ad
OBoladeras <oriolboladeras@gmail.com>
2023-06-30 07:41:57 -0400

    password rate feature add

--- app.py
+++ app.py
@@ -1,62 +1,73 @@
 from flask import Flask, render_template, redirect, url_for, request
-from password_manager import save_password, retrieve_password, generate_password
+from password_manager import (
+    save_password,
+    retrieve_password,
+    generate_password,
+    password_rate,
+)
 
 app = Flask(__name__)
-password_file = 'data.txt'
+password_file = "data.txt"
 
 
-@app.route('/')
+@app.route("/")
 def index():
     data = []
 
     try:
-        with open(password_file, 'r') as f:
+        with open(password_file, "r") as f:
             for line in f.readlines():
                 save = {}
-                save['web'] = line.split(',')[0]
-                save['username'] = line.split(',')[1]
+                save["web"] = line.split(",")[0]
+                save["username"] = line.split(",")[1]
 
                 data.append(save)
     except:
-        with open(password_file, 'w') as f:
-            f.write('')
+        with open(password_file, "w") as f:
+            f.write("")
 
-    return render_template('index.html', data=data)
+    return render_template("index.html", data=data)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index_post():
     requestMade = request.form.get("form_name")
 
-    if requestMade == 'save':
-        web = request.form['web']
-        username = request.form['username']
-        password = request.form['password']
+    if requestMade == "save":
+        web = request.form["web"]
+        username = request.form["username"]
+        password = request.form["password"]
 
         save_password(web, username, password)
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
 
-    elif requestMade == 'other':
-        web_username = request.form['show_password']
-        web, username = web_username.split('-')
+    elif requestMade == "other":
+        web_username = request.form["show_password"]
+        web, username = web_username.split("-")
 
         password = retrieve_password(web, username)
-        return render_template('serve.html', password=password)
+        return render_template("serve.html", password=password)
 
-    elif requestMade == 'generate_password':
-        with open(password_file, 'r') as f:
+    elif requestMade == "generate_password":
+        with open(password_file, "r") as f:
             data = []
 
             for line in f.readlines():
                 save = {}
-                save['web'] = line.split(',')[0]
-                save['username'] = line.split(',')[1]
+                save["web"] = line.split(",")[0]
+                save["username"] = line.split(",")[1]
 
                 data.append(save)
 
-        return render_template('index.html', data=data, password=generate_password())
+        rate = 0
+
+        while rate < 8:
+            password = generate_password()
+            rate = round(password_rate(password), 2)
+
+        return render_template("index.html", data=data, password=password, rate=rate)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = 18824
     app.run(port=port)
--- password_manager.py
+++ password_manager.py
@@ -3,18 +3,18 @@ import base64
 import hashlib
 from cryptography.fernet import Fernet
 
-password_file = 'data.txt'
+password_file = "data.txt"
 
 
 def get_encryption_key():
-    key_file = 'encryption_key.txt'
+    key_file = "encryption_key.txt"
 
     if os.path.exists(key_file):
-        with open(key_file, 'rb') as file:
+        with open(key_file, "rb") as file:
             encryption_key = file.read()
     else:
         encryption_key = generate_key()
-        with open(key_file, 'wb') as file:
+        with open(key_file, "wb") as file:
             file.write(encryption_key)
 
     return encryption_key
@@ -39,9 +39,8 @@ def decrypt_password(cipher_text, encryption_key):
 
 
 def hash_password(password):
-    salt = b'kb3p9sa8NSJ8783As'
-    hashed_password = hashlib.pbkdf2_hmac(
-        'sha256', password.encode(), salt, 100000)
+    salt = b"kb3p9sa8NSJ8783As"
+    hashed_password = hashlib.pbkdf2_hmac("sha256", password.encode(), salt, 100000)
 
     return hashed_password.hex()
 
@@ -51,22 +50,22 @@ def save_password(site, username, password):
 
     encrypted_password = encrypt_password(password, encryption_key)
     encoded_password = base64.urlsafe_b64encode(encrypted_password).decode()
-    with open(password_file, 'a') as file:
-        file.write(f'{site},{username},{encoded_password}\n')
+    with open(password_file, "a") as file:
+        file.write(f"{site},{username},{encoded_password}\n")
 
 
 def retrieve_password(site, username):
     encryption_key = get_encryption_key()
 
-    with open(password_file, 'r') as file:
+    with open(password_file, "r") as file:
         for line in file:
-            stored_site, stored_username, encoded_password = line.strip().split(',')
+            stored_site, stored_username, encoded_password = line.strip().split(",")
             if stored_site == site and stored_username == username:
-
-                encoded_password += '=' * (4 - len(encoded_password) % 4)
+                encoded_password += "=" * (4 - len(encoded_password) % 4)
                 encrypted_password = base64.urlsafe_b64decode(encoded_password)
                 decrypted_password = decrypt_password(
-                    encrypted_password, encryption_key)
+                    encrypted_password, encryption_key
+                )
 
                 return decrypted_password
 
@@ -74,8 +73,10 @@ def retrieve_password(site, username):
 def generate_password():
     import random
 
-    password = ''
-    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*-+=|:?,/"
+    password = ""
+    characters = (
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*-+=|:?,/"
+    )
 
     for i in range(random.randint(15, 20)):
         password += random.choice(characters)
@@ -84,3 +85,43 @@ def generate_password():
             password += str(i)
 
     return password
+
+
+def password_rate(password):
+    rate = 0
+    numbers = 0
+    symbols = 0
+    upper_case = 0
+    lower_case = 0
+
+    if len(password) <= 15:
+        rate = len(password) / 15
+        required = len(password) / 4 - 2
+    else:
+        rate = len(password) / 10
+        required = len(password) / 4
+
+    if rate > 2:
+        rate = 2
+
+    for character in str(password):
+        if character.isnumeric():
+            numbers += 1
+        elif character.isupper():
+            upper_case += 1
+        elif character.islower():
+            lower_case += 1
+        else:
+            symbols += 1
+
+    labels = [numbers, symbols, upper_case, lower_case]
+
+    for unit in labels:
+        if unit >= required:
+            rate += 2
+        elif unit == 0:
+            rate += 0
+        else:
+            rate += unit / required
+
+    return rate
